09a93b7e0506df4e9f39570d1a9e8bf155674331
Durgesh H <69630745+Durgesh-Tech@users.noreply.github.com>
2023-09-06 17:05:26 +0530

    Update showPageMap.js

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,15 +1,14 @@
-mapboxgl.accessToken = mapToken;//4.6
+mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    // style: 'mapbox://styles/mapbox/light-v10',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: campground.geometry.coordinates, //5.3
+    center: campground.geometry.coordinates, 
     zoom: 10
-});//4.6
+});
 
 map.addControl(new mapboxgl.NavigationControl());
 
-new mapboxgl.Marker()//5.1
+new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
