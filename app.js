371cf937fa01d847f95a3e271ebc1cc348d971e7
Durgesh H <69630745+Durgesh-Tech@users.noreply.github.com>
2023-09-06 19:20:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,9 +25,7 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
      useNewUrlParser: true,
-     useCreateIndex: true, 
-     useUnifiedTopology: true,
-     useFindAndModify: false, 
+     useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
