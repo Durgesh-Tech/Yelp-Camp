fdbdec55ecedfe8d9bd4caf2c09c283db9e50993
Durgesh H <69630745+Durgesh-Tech@users.noreply.github.com>
2023-09-06 17:12:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
      useNewUrlParser: true,
-     // useCreateIndex: true, it is not supported!!!
+     useCreateIndex: true, 
      useUnifiedTopology: true,
-     // useFindAndModify: false, it is not supported!!!
+     useFindAndModify: false, 
 });
 
 const db = mongoose.connection;
@@ -40,7 +40,7 @@ const app = express();
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))//first views represent the folder name, second views too...
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
@@ -69,7 +69,7 @@ const sessionConfig = {
      saveUninitialized: true,
      cookie: {
           httpOnly: true,
-          // secure: true,
+          secure: true,
           expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
           maxAge: 1000 * 60 * 60 * 24 * 7
      }
@@ -132,8 +132,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-     // console.log(req.session);
-     // console.log(req.query);
      res.locals.currentUser = req.user;
      res.locals.success = req.flash('success');
      res.locals.error = req.flash('error');
